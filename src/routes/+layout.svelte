013cf36acba0c971360da96be3f4dcd259a4a3bb
EMcNugget <ethan.d.seys@gmail.com>
2025-12-17 14:24:45 -0600

    Init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import './layout.css';
+  import favicon from '$lib/assets/favicon.svg';
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
+
+<main
+  class="relative flex min-h-screen flex-col items-center justify-center overflow-hidden
+         bg-linear-135 from-slate-950 via-blue-950 to-zinc-950 p-6"
+>
+  {@render children()}
+  <footer class="absolute bottom-0 p-4 text-sm text-slate-500">
+    <p>
+      Powered by <a
+        href="https://www.flightaware.com/commercial/aeroapi/"
+        class="text-blue-500 hover:underline">FlightAware</a
+      >
+    </p>
+  </footer>
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import * as Card from '$lib/components/ui/card/index.js';
+</script>
+
+<div class="mb-12 flex flex-col items-center gap-2">
+  <h1 class="text-4xl font-bold text-white text-shadow-lg">vBS - Virtual Bid System</h1>
+  <p class="font-semibold text-white text-shadow-lg">Your flight sim schedule made simple</p>
+</div>
+<Card.Root
+  class="
+      relative w-full max-w-md
+      rounded-2xl
+      border
+      border-white/15
+      bg-white/10 shadow-2xl
+      shadow-black/60 backdrop-blur-xl  
+      before:pointer-events-none before:absolute
+      before:inset-0
+      before:rounded-2xl before:bg-linear-to-br before:from-white/20 before:via-transparent
+      before:to-transparent
+      before:opacity-50
+    "
+>
+  <Card.Header class="border-b border-white/10">
+    <Card.Title class="text-xl text-white">Card Title</Card.Title>
+    <Card.Description class="text-slate-300">Card Description</Card.Description>
+  </Card.Header>
+
+  <Card.Content class="text-slate-200">
+    <p>Card Content</p>
+  </Card.Content>
+
+  <Card.Footer class="border-t border-white/10 text-slate-400">
+    <p>Card Footer</p>
+  </Card.Footer>
+</Card.Root>
